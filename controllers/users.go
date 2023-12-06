fd49314c4171e2293f78d7f7bf8588a27d2fd8b3
Thomas Loftus <thomasloftus@Thomass-MacBook-Pro.local>
2023-12-06 14:41:07 -0800

    forgot to commit controllers/users.go

--- controllers/users.go
+++ controllers/users.go
@@ -11,10 +11,12 @@ type Users struct {
 	}
 }
 
+// render signup form
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
 }
 
+// parse signup form
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Email: ", r.FormValue("email"))
 	fmt.Fprint(w, "Password: ", r.FormValue("password"))
