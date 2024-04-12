45b291a347de52542dcd040f249c59fb025fbc77
teelowe <thomloftus@gmail.com>
2024-04-11 19:26:45 -0700

    added Users.CurrentUser method to users controller

--- controllers/users.go
+++ controllers/users.go
@@ -57,11 +57,21 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	cookie := http.Cookie{
-		Name:  "Email",
+		Name:  "email",
 		Value: user.Email,
 		Path:  "/",
 	}
 	http.SetCookie(w, &cookie)
 
 	fmt.Fprintf(w, "user authenticated: %+v", user)
+}
+
+func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
+	email, err := r.Cookie("email")
+	if err != nil {
+		fmt.Fprint(w, "The email cookie could not be read.")
+		return
+	}
+	fmt.Fprintf(w, "Email cookie: %s\n", email.Value)
+	fmt.Fprintf(w, "Headers: %+v\n", r.Header)
 }
